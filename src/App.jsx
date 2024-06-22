971d1cf092edcc437eb3cb09546e5fc47cccaeef
Oliveira_Ju <107575747+J0li@users.noreply.github.com>
2024-06-22 16:41:51 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Portifolio from './Identidadevisual'
+
+function App() {
+
+  return (
+    <>
+      <Portifolio />
+    </>
+  )
+}
+
+export default App
---
+++ src/Identidadevisual.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import './IdVisual.css'
+
+function Portifolio() {
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    return (
+        <div>
+            <div className="Portifolio-body">
+                {selectedImage && (
+                    <div>
+                        <img className="img" alt="Selected Image" width="370px" height="340px"  src={URL.createObjectURL(selectedImage)} />
+
+                        <button className="btn-del" onClick={() => setSelectedImage(null)}>Remove</button>
+                    </div>
+                )}
+
+                <input className="add-img" type="file" name="myImage" onChange={(event) => {
+                    setSelectedImage(event.target.files[0]);
+                }} />
+            </div>
+
+        </div>
+    );
+};
+
+export default Portifolio;

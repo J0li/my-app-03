4742c6594722f3e33bde34b3dd354484f53ab0ec
Oliveira_Ju <107575747+J0li@users.noreply.github.com>
2024-06-22 17:01:24 -0300

    update01

--- src/Identidadevisual.jsx
+++ src/Identidadevisual.jsx
@@ -1,27 +1,86 @@
-import React, { useState } from "react";
-import './IdVisual.css'
+import '../assets/negocio.svg'
+import '../assets/Cores.svg'
+import '../assets/Cell_PhoneApp.svg'
 
-function Portifolio() {
-    const [selectedImage, setSelectedImage] = useState(null);
+
+function IdentVisual() {
+    const IdVisual = styled.div`
+margin: 0;
+`
+
+
+    const NossoNegocio = styled.div`
+    `
+
+    const VisaoFuturo = styled.div`
+    `
+
+    const tittles = styled.div`
+    `
+
+    const texts = styled.p`
+    `
+
+    const imgs = styled.img`
+margin: 0;
+`
+
 
     return (
         <div>
-            <div className="Portifolio-body">
-                {selectedImage && (
-                    <div>
-                        <img className="img" alt="Selected Image" width="370px" height="340px"  src={URL.createObjectURL(selectedImage)} />
+            <IdVisual>
 
-                        <button className="btn-del" onClick={() => setSelectedImage(null)}>Remove</button>
-                    </div>
-                )}
+                <tittles>
+                    <h3>nossas escolhas</h3>
+                    <H2>Identidade visual</H2>
+                </tittles>
 
-                <input className="add-img" type="file" name="myImage" onChange={(event) => {
-                    setSelectedImage(event.target.files[0]);
-                }} />
-            </div>
+                <imgs>
+                    <img src={Cores.svg} alt="Identidade Visual" />
+                </imgs>
+
+                <texts>
+                    <h3>Poppis</h3>
+                    <p>Design acolhedor, sua visualização proporciona um acolhimento ao usuário</p>
+                </texts>
+
+                <texts>
+                    <h3>Quantico</h3>
+                    <p>Design moderno, sua forma demonstrar algo tecnológico e moderno</p>
+                </texts>
+
+            </IdVisual>
+
+            <NossoNegocio>
+
+                <tittles>
+                    <h3>nossas escolhas</h3>
+                    <H2>Nosso negócio</H2>
+                </tittles>
+
+                <imgs>
+                    <img src={Nosso - negocio.svg} alt="Nosso negocio" />
+                </imgs>
+
+            </NossoNegocio>
+
+            <VisaoFuturo>
+
+                <tittles>
+                    <h3>E não para por aí</h3>
+                    <h2>Futuros passos</h2>
+                </tittles>
+
+                <texts>
+                    <p>Um aplicativo para dispositiveis moveis, tanto para o sistema android como IOS</p>
+                </texts>
+
+                <imgs>
+                    <img src={Cell_PhoneApp.svg} alt="Aplicativos Mobile" />
+                </imgs>
+
+            </VisaoFuturo>
 
         </div>
-    );
-};
-
-export default Portifolio;
+    )
+}

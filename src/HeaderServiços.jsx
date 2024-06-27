9530d01f437f3f41d0eb89006d483e9cf7fcb406
Oliveira_Ju <107575747+J0li@users.noreply.github.com>
2024-06-26 23:32:24 -0300

    .

--- src/HeaderServiços.jsx
+++ src/HeaderServiços.jsx
@@ -6,7 +6,16 @@ function Servicos() {
     const Servheader = styled.div`
     width:100%;
 position: relative;
+
+@media(max-width:992px;){
+flex-direction: column;
+img{
+width: 100%;
+}
+}
 `
+
+
 
     const ImagemS = styled.div`
     width: 100%;
@@ -16,6 +25,7 @@ position: relative;
 
     const ConteinerOpcao = styled.div`
     position: absolute;
+    align-items: center;
 `
 
 
@@ -25,7 +35,7 @@ position: relative;
         /* espaço navbar */
 
             <ConteinerOpcao>
-                <p>Venha fazer parte da nossa plataforma, sendo cliente ou prestador de serviços. </p>
+                <p>Venha fazer parte da nossa plataforma, sendo cliente ou prestador de serviços.</p>
                 <button style={{ color: '#FB8C00' }}>Cliente</button>
                 <button style={{ color: '#2CB87B' }}>Profissional</button>
             </ConteinerOpcao>
